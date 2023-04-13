72bd7effa73147ce1e71b57abc843913f703cecf
Dmitriy Obrazumov <obrazumovd@mail.ru>
2023-04-13 15:58:14 +0300

    initialCommit

---
+++ Sources/ODDependencyInjection/DIManager.swift
@@ -0,0 +1,42 @@
+//
+//  DIManager.swift
+//  
+//
+//  Created by Dmitriy Obrazumov on 07/04/2023.
+//
+
+import Foundation
+
+struct DependencyInjector {
+    private static var dependencyList: [String:Any] = [:]
+    
+    static func resolve<T>() -> T {
+        guard let t = dependencyList[String(describing: T.self)] as? T else {
+            fatalError("No provider registered for type \(T.self)")
+        }
+        return t
+    }
+    
+    static func register<T>(dependency: T) {
+        dependencyList[String(describing: T.self)] = dependency
+    }
+}
+
+@propertyWrapper struct Inject<T> {
+    var wrappedValue: T
+    
+    init() {
+        self.wrappedValue = DependencyInjector.resolve()
+        print("Injected <-", self.wrappedValue)
+    }
+}
+
+@propertyWrapper struct Provider<T> {
+    var wrappedValue: T
+    
+    init(wrappedValue: T) {
+        self.wrappedValue = wrappedValue
+        DependencyInjector.register(dependency: wrappedValue)
+        print("Provided ->", self.wrappedValue)
+    }
+}
